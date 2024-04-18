26c3c02a970058b369a14eaed642325d168f59d9
Moderocky <admin@moderocky.com>
2024-04-18 14:24:54 +0100

    Test for mathematical and semantic equivalency.

--- src/test/java/org/valross/logic/lukasiewicz/LukNValueTest.java
+++ src/test/java/org/valross/logic/lukasiewicz/LukNValueTest.java
@@ -6,16 +6,18 @@ import org.valross.logic.ValueTest;
 
 import java.util.Random;
 
+import static org.valross.logic.lukasiewicz.LukasiewiczLogic.MANY_VALUED;
+
 public class LukNValueTest extends ValueTest {
 
     @Override
     protected Value truth() {
-        return LukasiewiczLogic.MANY_VALUED.truth();
+        return MANY_VALUED.truth();
     }
 
     @Override
     protected Value falsity() {
-        return LukasiewiczLogic.MANY_VALUED.falsity();
+        return MANY_VALUED.falsity();
     }
 
     @Test
@@ -30,6 +32,19 @@ public class LukNValueTest extends ValueTest {
             assert a.implies(b).implies(b).implies(b.implies(a).implies(a)).floatValue() == 1;
             assert b.not().implies(a.not()).implies(a.implies(b)).floatValue() == 1;
             assert a.not().not().implies(a).floatValue() == 1;
+        }
+    }
+
+    @Test
+    public void equivalency() {
+        final Random random = new Random();
+        for (int i = 0; i < 100; i++) {
+            final LukValue a = new LukNValue(random.nextFloat()), b = new LukNValue(random.nextFloat());
+            assert a.implies(b).floatValue() == MANY_VALUED.implies(a, b).floatValue();
+            assert a.equals(b).floatValue() == MANY_VALUED.equals(a, b).floatValue();
+            assert a.not().floatValue() == MANY_VALUED.not(a).floatValue();
+            assert a.or(b).floatValue() == MANY_VALUED.or(a, b).floatValue();
+            assert a.and(b).floatValue() == MANY_VALUED.and(a, b).floatValue();
         }
     }
 
